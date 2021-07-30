10a19c6e8d81ce467b831ef1d7636614231ab5b4
white9p <kmjkmj9905@naver.com>
2021-07-30 19:30:31 +0900

    feat: 주석을 좀더 자세히 변경하고 일반함수를 화살표 함수로 전환하였다.

--- week2/main.js
+++ week2/main.js
@@ -28,20 +28,18 @@ alert(msg);
 
 // 아래에 getItemByAmount 함수를 작성하세요.
 function getItemByAmount(data, amount){
-    if(isNaN(amount)){ //숫자가 아닌 다른 것을 입력 했을 때
+    if(isNaN(amount)){ //숫자가 아닌 다른 것을 입력 했을 때 null을 반환한다.
         return null;
     }
-    //데이터 가격 기준 오름차순 정렬
-    data.sort(function(a,b) {
-        return a.price-b.price;
-    });
+    //데이터 가격 기준 오름차순 정렬 (더 저렴한 순부터 비싼 순으로)
+    data.sort((a,b) => a.price - b.price);
     
     for(let i=0; i<data.length; i++) {
-        if (amount > data[i].price) { // 입력한 amount가 상품가격보다 클때 다음 상품으로 넘어간다.
+        if (amount > data[i].price) { // 사용자가 입력한 amount가 상품가격보다 클때 다음 상품으로 넘어간다.
             continue;
         } else if (amount == data[i].price) {  // 입력한 amount가 상품 가격과 같을 때 해당상품 반환
             return data[i];
-        } else { //입력한 amount가 상품 가격보다 작을 때 이전 상품을 반환
+        } else { //입력한 amount가 상품 가격보다 작을 때 이전 상품을 반환 (만약 amount가 첫번째 해당 상품보다 가격이 작으면 data[-1]이 되고 그 값이 undefined가 되므로 falsy한 값이 됨)
             return data[i-1];
         }    
     }
