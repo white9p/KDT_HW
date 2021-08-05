7be98c04d96df8a8f55433b46290cd6fa6ba52ed
white9p <kmjkmj9905@naver.com>
2021-08-05 22:15:35 +0900

    feat: Change email info on email_list.xlsx and completed mail sending

--- week3/SendingEmail.py
+++ week3/SendingEmail.py
@@ -5,25 +5,23 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import smtplib
 import re
-# 사용자로 부터 기사 수집을 원하는 키워드를 input을 이용해 입력받아 ? 부분에 넣으세요
-crawler = NaverNewsCrawler("설탕")  # TODO 추후 대체 예정
+# 사용자에게 뉴스 키워드 검색어를 입력받는다.
+user_input = input('검색어를 입력하세요:')
+crawler = NaverNewsCrawler(user_input)
 
-# 수집한 데이터를 저장할 엑셀 파일명을 input을 이용해 입력받아 ? 부분에 넣으세요
-
-crawler.get_news("news_result.xlsx")
-
-# 아래코드를 실행해 이메일 발송 기능에 필요한 모듈을 임포트하세요.
+# 수집한 데이터를 저장할 엑셀 파일명을 input을 이용해 입력받아 crawler.get_news 부분에 넣는다
+excel_filename = input('엑셀 파일명을 입력하세요:')
+crawler.get_news(excel_filename + ".xlsx")
 
 
-# gmail 발송 기능에 필요한 계정 정보를 아래 코드에 입력하세요.
+# gmail 발송 기능에 필요한 계정 정보를 입력
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 465
-SMTP_USER = 'samakcottontail@gmail.com'
-SMTP_PASSWORD = 'thaxjf92'
-
-# 아래 코드를 실행해 메일 발송에 필요한 send_mail 함수를 만드세요.
+SMTP_USER = 'fastcampus@gmail.com'
+SMTP_PASSWORD = ''  # 보안상 쓰지 않았습니다:)
 
 
+# 메일 발송에 필요한 send_mail 함수
 def send_mail(name, addr, subject, contents, attachment=None):
     if not re.match('(^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)', addr):
         print('Wrong email')
@@ -61,15 +59,18 @@ def send_mail(name, addr, subject, contents, attachment=None):
 # 프로젝트 폴더에 있는 email_list.xlsx 파일에 이메일 받을 사람들의 정보를 입력하세요.
 
 
-# 엑셀 파일의 정보를 읽어올 수 있는 모듈을 import하세요.
+# email_list.xlsx 파일의 이름과 메일정보 가져오기
 wb = load_workbook('email_list.xlsx', read_only=True)
 data = wb.active
 
+# enumerate 함수로 email_list의 데이터들을 3열 까지만 행단위로 가져와서 인덱스 번호와 컬렉션의 원소를 tuple형태로 반환
 for idx, row in enumerate(data.iter_rows(max_col=3)):
-    if idx == 0:
+    if idx == 0:  # 거기서 for문으로 첫번째 행은 머릿말이니 무시하고 넘어가게 조건문을 걸고
         continue
-    print(row[1].value)
-    print(row[2].value)
+    receiver_name = row[1].value  # 행마다의 두번째 열에있는 이름 값을 변수에 넣기
+    receiver_email = row[2].value  # 행마다의 세번째 열에있는 이메일 주소 값을 변수에 넣기
+    subject = "뉴스 결과 보고 드립니다."  # 제목에 해당하는 변수
+    contents = receiver_name + "님, " + user_input+"에 대한 결과입니다."  # 이메일 내용에 해당하는 변수
+    attachment = excel_filename + ".xlsx"  # 만들어질 파일이름 변수
 
-# email_list.xlsx 파일을 읽어와 해당 사람들에게 수집한 뉴스 정보 엑셀 파일을 send_mail 함수를 이용해 전송하세요.
-send_mail()
+    send_mail(receiver_name, receiver_email, subject, contents, attachment)
